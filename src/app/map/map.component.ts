f2b196f70fa37a1c31560d836c0b7bf7c68e6bb4
Tymofieiev <vovanium.timofeev@gmail.com>
2021-05-04 20:59:53 +0300

    2 layers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,18 +11,35 @@ export class MapComponent implements AfterViewInit {
   private map;
 
   private initMap(): void {
+
+    // init map
     this.map = L.map('map', {
       center: [35.033333, 48.45],
       zoom: 3
     });
 
+    // tiles
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
+    const googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+
+    // tiles object
+    const baseMap = {
+      OSM: tiles,
+      Google: googleStreets,
+    };
+
     tiles.addTo(this.map);
+
+    // adding two layers
+    L.control.layers(baseMap).addTo(this.map);
   }
 
 
